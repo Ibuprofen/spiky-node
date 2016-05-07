2f5283c4cf1ac780e106dfd742b0e923637d7472
Tony Rieker <tony@plenary.org>
2016-05-07 00:19:00 -0700

    a working test

--- src/main.ino
+++ src/main.ino
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUDP.h>
+#include <SD.h>
 
 #include "Secrets.h"
 
@@ -11,6 +12,8 @@ IPAddress netMsk(255, 255, 255, 0);
 WiFiUDP UDP;
 #define UDP_PORT 8888
 
+// SD
+File root;
 
 void setup() {
   Serial.begin(115200);
@@ -35,6 +38,19 @@ void setup() {
   }
   Serial.println("UDP ready. ");
 
+
+  // SD
+  Serial.print("Initializing SD card...");
+
+  // put the SD card on the HSPI pins D5-D8
+  if (!SD.begin()) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+
+  printRoot();
+
 }
 
 void loop() {
@@ -46,5 +62,45 @@ void loop() {
     unsigned char c = UDP.read();
     Serial.println(c);
     avail = avail - 1;
+  }
+
+
+
+
+}
+
+
+void printRoot() {
+  root = SD.open("/");
+
+  printDirectory(root, 0);
+
+  Serial.println("done!");
+}
+
+
+void printDirectory(File dir, int numTabs) {
+  while (true) {
+
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      Serial.println("No more files. rewinding.");
+      //dir.rewindDirectory();
+      // no more files
+      break;
+    }
+    for (uint8_t i = 0; i < numTabs; i++) {
+      Serial.print('\t');
+    }
+    Serial.print(entry.name());
+    if (entry.isDirectory()) {
+      Serial.println("/");
+      printDirectory(entry, numTabs + 1);
+    } else {
+      // files have sizes, directories do not
+      Serial.print("\t\t");
+      Serial.println(entry.size(), DEC);
+    }
+    entry.close();
   }
 }
